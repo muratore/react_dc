c015ecb139631696376415e2b77256cb396d8664
Alessandro Muratore <muratore@gmail.com>
2023-04-17 21:47:33 -0300

    Fazendo a Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Input from './components/Input'
-import Title from './components/Title'
+// import "./App.css";
+import Input from "./components/Input";
+import Title from "./components/Title";
+import NavBar from "./components/NavBar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <Title title='Olá você conseguiu'/>
-     <Input placeholder='Nome' name='name' id='name'/>
-     <Input placeholder='Sobrenome' name='lastName' id='lastName'/>
+    <div>
+      <NavBar/>
+      {/* <Title title="Olá você conseguiu" />
+      <Input placeholder="Nome" name="name" id="name" />
+      <Input placeholder="Sobrenome" name="lastName" id="lastName" /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
